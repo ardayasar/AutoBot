76c8fb1976a43a21a4dc2526f4c1f46c9ae08964
ardayasar <erddoerdo12@gmail.com>
2023-11-02 16:19:21 +0300

    Chrome driver changed

--- test.py
+++ test.py
@@ -1,48 +1,24 @@
 import requests
-import json
-from selenium.webdriver.common.by import By
-import time
-import undetected_chromedriver as uc
-
-options = uc.ChromeOptions()
-# options.add_argument('--headless=new')
-options.headless = False
-driver = uc.Chrome(use_subprocess=True, options=options)
-driver.maximize_window()
-
-driver.get("https://www.turkanime.co/")
-time.sleep(3)
-cl = driver.get_cookies()
-print(cl)
-
-# Filter and prepare cookies for requests
-desired_cookies = ['cf_clearance', 'PHPSESSID']
-cookies_dict = {}
-for c in cl:
-    if c['name'] in desired_cookies:
-        cookies_dict[c['name']] = c['value']
 
 url = "https://www.turkanime.co/"
 
-payload = {}
+payload={}
 headers = {
-    'Host': 'www.turkanime.co',
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:109.0) Gecko/20100101 Firefox/119.0',
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
-    'Accept-Language': 'tr-TR,tr;q=0.8,en-US;q=0.5,en;q=0.3',
-    'Accept-Encoding': 'gzip, deflate, br',
-    'Connection': 'keep-alive',
-    'Cookie': 'cf_clearance=.gn.zf260EYCOuDWgMwG_53.uEw4e6BK7B9dmxllmf0-1698926138-0-1-5333897d.d646b197.432d69d-250.2.1698926138; PHPSESSID=8qbstno7ocuqvofchqbcm0it1m',  # Include desired cookies in the "Cookie" header
-    'Upgrade-Insecure-Requests': '1',
-    'Sec-Fetch-Dest': 'document',
-    'Sec-Fetch-Mode': 'navigate',
-    'Sec-Fetch-Site': 'none',
-    'Sec-Fetch-User': '?1',
-    'x-postman-captr': '9535798'
+  'host': 'www.turkanime.co',
+  'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:109.0) Gecko/20100101 Firefox/119.0',
+  'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
+  'accept-language': 'tr-TR,tr;q=0.8,en-US;q=0.5,en;q=0.3',
+  'accept-encoding': 'gzip, deflate, br',
+  'connection': 'keep-alive',
+  'cookie': 'cf_clearance=jHovKfcDmZaCkJs19oPyk6IaXf45MGMLzjF3pnzr2i0-1698930985-0-1-5333897d.d646b197.432d69d-250.2.1698930985; PHPSESSID=8qbstno7ocuqvofchqbcm0it1m',
+  'upgrade-insecure-requests': '1',
+  'sec-fetch-dest': 'document',
+  'sec-fetch-mode': 'navigate',
+  'sec-fetch-site': 'none',
+  'sec-fetch-user': '?1',
+  'x-postman-captr': '9409557'
 }
 
-response = requests.get(url, headers=headers, data=payload)
+response = requests.request("GET", url, headers=headers, data=payload)
 
-print(response)
-
-driver.quit()
+print(response.text)
